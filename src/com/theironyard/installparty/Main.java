2796da884b0101f7fc33725f8c421eab705ac3ee
EliGarcia <garciaelisha93@yahoo.com>
2017-09-11 11:42:58 -0500

    unable to end the game

---
+++ src/com/theironyard/installparty/Main.java
@@ -0,0 +1,37 @@
+package com.theironyard.installparty;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double ranNum = Math.floor(Math.random() * 100);
+        System.out.println(ranNum);
+
+        System.out.println("Guess a number between 1 and 100!");
+        Scanner scanner = new Scanner(System.in);
+        int howManyGuesses = 100;
+        int guessesMade = 0;
+        double userGuess = -1;
+        boolean winner = false;
+
+        if (winner == false) {
+
+            while (guessesMade != howManyGuesses) {
+                userGuess = scanner.nextInt();
+                guessesMade++;
+                if (ranNum == userGuess) {
+                    System.out.println(userGuess + " is correct!");
+                    winner = true;
+
+                } else if (ranNum < userGuess) {
+                    System.out.println(userGuess + " is too high!");
+                } else if (ranNum > userGuess) {
+                    System.out.println(userGuess + " is too low!");
+                }
+            }
+        } else {
+            System.out.println("you took " + guessesMade + " guesses to win");
+        }
+    }
+}
